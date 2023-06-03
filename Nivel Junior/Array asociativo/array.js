9b0224dc41482e178b9f710cce116c0e2ba9036a
Shuyin <shuyin.github@gmail.com>
2023-06-03 06:09:28 -0400

    ejemplo de array

---
+++ Nivel Junior/Array asociativo/array.js
@@ -0,0 +1,47 @@
+
+//? pc 1 es un array normal
+
+ let pc1 = ["pc1" , "intel core i3" ,  "ram 8gb" , "500gb"] ;       
+
+// ? pc2 es un array asociativo
+
+ let pc2 = {                                                      
+    nombre : "PC 2" ,
+    procesador : "Intel core i7" ,
+    ram : "16GB ram" ,
+    espacio : "2TB"
+ } ;   
+
+
+
+
+
+
+
+
+
+
+
+// ! comentario descriptivo abajo
+
+
+ 
+ 
+ // ? Un array asociativo, también conocido como objeto o diccionario, es una estructura de datos que permite almacenar pares de clave-valor. A diferencia de un array tradicional, donde los elementos se acceden mediante un índice numérico, en un array asociativo los elementos se acceden a través de claves o nombres descriptivos.
+
+ // ? Cada propiedad se define mediante la sintaxis clave: valor. En este caso, las claves son nombres descriptivos y los valores son cadenas de texto que representan las características de un computador.
+
+// * Para acceder a los valores almacenados en un objeto, se utiliza la notación de punto (objeto.propiedad) o la notación de corchetes (objeto["propiedad"]), donde "objeto" es el nombre de la variable que contiene el objeto y "propiedad" es el nombre de la propiedad a la que se desea acceder.
+
+// * Por ejemplo, para obtener el valor de la propiedad nombre del objeto pc2, puedes usar pc2.nombre, lo cual te devolverá "PC 2". Del mismo modo, puedes acceder a las demás propiedades de pc2 de la siguiente manera:
+
+/* 
+
+pc2.procesador devuelve "Intel core i7"
+pc2.ram devuelve "16GB ram"
+pc2.espacio devuelve "2TB"
+
+*/
+
+
+// ? En resumen, un array asociativo u objeto en JavaScript es una estructura de datos que almacena pares de clave-valor. Se puede acceder a los valores utilizando la notación de punto o la notación de corchetes. pc2 es un objeto que representa las características de un computador.
---
+++ Nivel Junior/Bucle for/for.js
@@ -0,0 +1,19 @@
+// ? El bucle for en JavaScript se utiliza para repetir una sección de código un número específico de veces. Tiene tres partes principales: la inicialización, la condición y la actualización. de esta forma:
+
+for (inicialización; condición; actualización) {
+    // Código a ejecutar en cada iteración
+  }
+ 
+// ! Ejemplo:
+
+for (let i = 0; i < 5; i++) {
+    console.log("El valor de i es: " + i);
+  }
+  
+// * Inicialización: En la inicialización, se declara una variable de control y se le asigna un valor inicial. Esta parte del bucle se ejecuta solo una vez al inicio. Por lo general, se utiliza la palabra clave let para declarar la variable, seguida del nombre de la variable y su valor inicial.
+
+// * Condición: La condición se evalúa antes de cada iteración del bucle. Si la condición es verdadera, el código dentro del bucle se ejecuta. Si la condición es falsa, el bucle se detiene y el control pasa a la siguiente línea de código después del bucle. La condición puede ser cualquier expresión que se pueda evaluar como verdadera o falsa.
+
+// * Actualización: En la actualización, se modifica la variable de control después de cada iteración. Por lo general, se incrementa o decrementa el valor de la variable para avanzar hacia la condición de finalización. Esta parte se ejecuta después de cada iteración antes de que se evalúe la condición nuevamente.
+
+
