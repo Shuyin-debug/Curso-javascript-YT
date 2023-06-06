26d7ff0aa01041aec6ea8e99d4bd92658ea0a90d
Shuyin <shuyin.github@gmail.com>
2023-06-06 03:08:57 -0400

    POO creado

--- Nivel Junior/Programacion orientada a objetos/POO.js
+++ Nivel Junior/Programacion orientada a objetos/POO.js
@@ -12,14 +12,42 @@ class animal {
     verInfo() {  
         document.write(this.info + "<br>")
     }
-    
+
 }
 
 
-let perro = new animal("perro" , 5 , "marron") ;
-let gato = new animal ("gato" , 2 , "gris") ;
-let pajaro = new animal ("loro" , 1 , "verde")
+// ? la clase perro es una herencia de la clase animal
+class perro extends animal {
+    constructor(especie,edad,color,raza){
+        super(especie,edad,color);
+        this.raza = raza ;
+    }
 
-perro.verInfo() ;
+    set setRaza(newName) {  // ? el set lo toma como propiedad
+        this.raza = newName ;
+    }
+
+    get getRaza(){  // ? el get se utiliza para tomar un valor
+        return this.raza;
+    }
+
+}
+
+// ! una variable no puede tener el mismo nombre que una clase , tambien las clases podrian tener mayuscula al inicio para no tener conflico con el nombre de la variable
+
+// ! las instancias o objetos deben de comenzar con const
+
+const perroo = new animal("perro" , 5 , "marron" , "doberman") ;
+const gato = new animal ("gato" , 2 , "gris") ;
+const pajaro = new animal ("loro" , 1 , "verde")
+
+
+
+// * Los getters se utilizan para obtener el valor de una propiedad, mientras que los setters se utilizan para establecer el valor de una propiedad. 
+perroo.setRaza = "Husky" ;
+document.write(perroo.getRaza) ;
+
+
+perroo.verInfo() ;
 gato.verInfo() ;
 pajaro.verInfo() ;
