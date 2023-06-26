9bb6146ce2f3bd00044bc11595951282270ffa2a
Shuyin <shuyin.github@gmail.com>
2023-06-26 16:44:41 -0400

    Objeto Math

---
+++ Nivel Junior/Objeto Math/math.js
@@ -0,0 +1,60 @@
+
+
+//* Genera un número aleatorio entre 0 y 1
+let aleatorio = Math.random();
+console.log(aleatorio); // Output: un número aleatorio entre 0 y 1
+
+//* Redondea un número hacia arriba
+let numero1 = 3.2;
+let redondeado1 = Math.ceil(numero1);
+console.log(redondeado1); // Output: 4
+
+//* Redondea un número hacia abajo
+let numero2 = 3.8;
+let redondeado2 = Math.floor(numero2);
+console.log(redondeado2); // Output: 3
+
+//* Calcula la raíz cuadrada de un número
+let numero3 = 25;
+let raiz = Math.sqrt(numero3);
+console.log(raiz); // Output: 5
+
+//* Calcula el valor absoluto de un número
+let numero4 = -5;
+let absoluto = Math.abs(numero4);
+console.log(absoluto); // Output: 5
+
+//* Devuelve el entero más pequeño mayor o igual que un número
+let numero5 = 4.2;
+let entero1 = Math.ceil(numero5);
+console.log(entero1); // Output: 5
+
+//* Devuelve el entero más grande menor o igual que un número
+let numero6 = 4.8;
+let entero2 = Math.floor(numero6);
+console.log(entero2); // Output: 4
+
+//* Eleva un número a una potencia
+let base = 2;
+let exponente = 3;
+let potencia = Math.pow(base, exponente);
+console.log(potencia); // Output: 8
+
+//* Devuelve el seno de un ángulo en radianes
+let angulo = Math.PI / 2;
+let seno = Math.sin(angulo);
+console.log(seno); // Output: 1
+
+//* Devuelve el coseno de un ángulo en radianes
+let angulo2 = 0;
+let coseno = Math.cos(angulo2);
+console.log(coseno); // Output: 1
+
+//* Devuelve la tangente de un ángulo en radianes
+let angulo3 = Math.PI / 4;
+let tangente = Math.tan(angulo3);
+console.log(tangente); // Output: 0.9999999999999999
+
+//* Devuelve un número aleatorio entre 0 y 1
+let aleatorio2 = Math.random();
+console.log(aleatorio2); // Output: un número aleatorio entre 0 y 1
