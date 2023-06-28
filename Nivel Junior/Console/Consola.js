b98920556430d8c9b89cbdbbbc92c87fffbc0445
Shuyin <shuyin.github@gmail.com>
2023-06-28 14:12:09 -0400

    Consola

---
+++ Nivel Junior/Console/Consola.js
@@ -0,0 +1,63 @@
+
+                                        ///* Funciones de Registro ///
+                            
+
+console.assert() // ? Se utiliza para verificar si una afirmación es verdadera , es útil para hacer pruebas rápidas y verificar que el código esté funcionando correctamente.
+
+
+console.log() // ? Imprime un mensaje en la consola del navegador. Es útil para depurar y verificar el valor de variables y objetos.
+
+
+console.error() // ? Muestra un mensaje de error en la consola del navegador. Es útil para indicar que ha ocurrido un error en el código.
+
+
+console.warn() // ? Muestra un mensaje de advertencia en la consola del navegador. Es útil para indicar que algo puede no estar funcionando correctamente.
+
+
+console.table() // ? Muestra un arreglo o un objeto en forma de tabla en la consola del navegador. Es útil para visualizar datos de manera ordenada.
+
+
+console.group() // ? Crea un grupo de mensajes en la consola del navegador. Es útil para agrupar mensajes relacionados y hacer la consola más fácil de leer.
+
+
+console.clear() // ? Limpia la consola del navegador. Es útil para limpiar la consola después de hacer muchas pruebas y depuraciones.
+
+
+console.info() //? Muestra un mensaje informativo en la consola del navegador. Es útil para mostrar información adicional sobre el funcionamiento del código. A menudo se utiliza para imprimir información sobre el estado de la aplicación o para proporcionar detalles sobre cómo usar una función o método en particular.
+
+
+console.dir() //? Muestra una lista interactiva de las propiedades de un objeto en la consola del navegador. Es útil para explorar la estructura de un objeto y ver sus propiedades y métodos. La salida de console.dir() es más detallada que la de console.log(), y puede ser especialmente útil para depurar objetos complejos o anidados.
+
+
+                                                   /// * Funciones de conteo ///
+
+
+console.count() // ? Registra el número de veces que se llama a console.count() con un identificador específico en la consola del navegador. Es útil para contar cuántas veces se ha ejecutado un bucle o una función.
+
+
+console.countReset() // ? Reinicia el contador para un identificador específico a cero en la consola del navegador. Es útil para reiniciar el contador y comenzar a contar de nuevo desde cero.                                      
+
+
+
+                                                 //* Funciones de agrupacion //
+
+
+console.group() // ? Crea un grupo de mensajes en la consola del navegador. Es útil para agrupar mensajes relacionados y hacer la consola más fácil de leer. se puede nombre el grupo poniendo adentro "nombreDeGrupo"
+
+
+console.groupEnd() // ? Finaliza el grupo de mensajes creado con console.group() en la consola del navegador.
+
+
+console.groupCollapsed() // ? Crea un grupo de mensajes colapsado en la consola del navegador. Es útil para agrupar mensajes relacionados y hacer la consola más fácil de leer, pero ocultando el contenido del grupo por defecto. El usuario puede expandir el grupo haciendo clic en él.
+
+
+///* Funciones de temporizacion ///
+
+
+console.time() // ? Inicia un temporizador en la consola del navegador. Es útil para medir el tiempo que tarda en ejecutarse una sección de código.
+
+
+console.timeEnd() // ? Detiene el temporizador iniciado con console.time() y muestra el tiempo que tardó en ejecutarse la sección de código.
+
+console.timeLog() // ? Muestra el tiempo transcurrido desde que se inició el temporizador creado con console.time() en la consola del navegador. Es útil para imprimir el tiempo transcurrido en diferentes puntos de una sección de código y diagnosticar posibles cuellos de botella.
+
